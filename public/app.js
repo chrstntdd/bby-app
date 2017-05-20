f785b107f0958c7f77c7ea33bdf35f5f46090f04
Christian Todd <christiantodd27@icloud.com>
2017-05-20 17:48:52 -0400

    Remove Ramda, replace with lodash, render data to screen.

--- public/app.js
+++ public/app.js
@@ -1,23 +1,93 @@
-// VALID UPC = 888462313674
+$(() => {
+  handleUPCInput();
+  handleDecrementQty();
+  handleRemoveItem();
+})
 
-let clientUPC = document.getElementById('upc');
+const state = {
+  products: []
+};
 
-let validUPC = () => {
-  let clientUPCValue = R.trim(clientUPC.value)
-  clientUPCValue.length == 12 ? callBbyAPI(clientUPCValue) : console.log('invalid UPC');
+let $clientUPC = $('#upc');
+
+const validUPC = () => {
+  let clientUPCValue = _.trim($clientUPC.val())
+  clientUPCValue.length == 12 ?
+    callBbyAPI(clientUPCValue) :
+    console.log('Invalid UPC');
 }
 
-let callBbyAPI = (clientUPCValue) => {
-  console.log('Valid UPC!');
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = () => {
-    if (xhr.status !== 200){
-      alert('Request failed. Returned status of ' + xhr.status);
+const callBbyAPI = (clientUPCValue) => {
+  $.post('/', `upc=${clientUPCValue}`, data => {
+    if (doesExist(data)) {
+      let product = _.find(state.products, data);
+      product.quantity++;
+    } else {
+      data.quantity = 1;
+      state.products.push(data);
     }
-  }
-  xhr.send('upc=' + clientUPCValue);
+    setTimeout(() => $clientUPC.val(''), 500);
+
+    renderProduct(data);
+  });
 }
 
-clientUPC.addEventListener('input', validUPC);
+const doesExist = data => {
+  if (!_.some(state.products, data)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const handleUPCInput = () => {
+  $clientUPC.on('input', () => {
+    validUPC();
+  });
+}
+
+
+const renderProduct = (data) => {
+  let productDetailsHTML = (
+    '<li>' +
+    '<p class="name"></p>' +
+    '<p class="sku"></p>' +
+    '<p class="upc"></p>' +
+    '<p class="department"></p>' +
+    '<p class="class"></p>' +
+    '<p class="quantity"></p>' +
+    '<button class="decrement-quantity">-1</button>' +
+    '<button class="remove-item">remove item</button>' +
+    '</li>'
+  );
+
+  let $temp = $(productDetailsHTML);
+  let sku = '#' + data.sku;
+
+  $temp.attr('id', data.sku);
+  $temp.find('.name').text(data.name);
+  $temp.find('.sku').text(data.sku);
+  $temp.find('.upc').text(data.upc);
+  $temp.find('.department').text(data.department);
+  $temp.find('.class').text(data.class);
+  $temp.find('.quantity').text(data.quantity);
+
+  if ($(sku).length) {
+    $productQty = parseInt($(sku + ' .quantity').text());
+    $(sku + ' .quantity').text($productQty + 1);
+  } else {
+    $('#product-list').append($temp);
+  }
+}
+
+const handleRemoveItem = () => {
+  $('.remove-item').on('click', () => {
+    //
+  })
+}
+
+const handleDecrementQty = () => {
+  $('.decrement-quantity').on('click', () => {
+    //
+  })
+}
