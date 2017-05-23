89a6840862d345993c8071e59632498beeea7d51
Christian Todd <christiantodd27@icloud.com>
2017-05-23 00:29:28 -0400

    uglify all of the js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: ['babel-polyfill','./public/app.js'],
@@ -6,9 +7,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
+  plugins: [
+    new UglifyJSPlugin()
+  ],
   module: {
   rules: [
     { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-  ]
+  ],
 }
 };
